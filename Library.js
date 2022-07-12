668976f6be88c7d693ad1541c4d9203dc30069ce
Mark Procopio <77703070+procopim@users.noreply.github.com>
2022-07-12 12:53:55 -0400

    Update Library.js with comments

--- Library.js
+++ Library.js
@@ -56,9 +56,9 @@ class Movie extends Media{
   }
 }
 
-const NinteenEightyFour = new Book('Orwell','1984',350);
-
+// create new Book class instance
 const historyOfEverything = new Book('Bill Bryson','A Short History of Nearly Everything',544);
+
 historyOfEverything.toggleCheckOutStatus();
 console.log(historyOfEverything.isCheckedOut)
 historyOfEverything.addRating(4)
@@ -66,6 +66,7 @@ historyOfEverything.addRating(5)
 historyOfEverything.addRating(5)
 console.log(historyOfEverything.getAverageRating())
 
+//create new Movie class instance
 const speed = new Movie('Jan de Bont','Speed',116)
 speed.toggleCheckOutStatus()
 console.log(speed.isCheckedOut)
